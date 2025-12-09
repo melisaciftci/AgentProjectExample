c02f14512b5b4747df713ee30ee8f69770bdb30c
Melisa Ciftci <ciftcimelisa03@gmail.com>
2025-12-09 18:55:04 +0300

    integrating agent for better response

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import OpenAI, ChatOpenAI
 from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent # ReAct agent türünü oluşturuyor
 
 load_dotenv()
 
@@ -11,9 +12,13 @@ search = TavilySearchResults(max_results=2)
 
 tools=[search]
 
-model_with_tools = model.bind_tools(tools)
+# model_with_tools = model.bind_tools(tools) -> agent_executor ile yaptığımız için buna gerek kalmadı
+
+agent_executor = create_react_agent(model, tools) # model ve tool birbirine bağlanıyor
 
 if __name__=="__main__":
-    response = model_with_tools.invoke([HumanMessage(content="what is the current weather in adana now?")])
-    print(response.content)
-    print(response.tool_calls)
+    response = agent_executor.invoke(
+        {"messages": [HumanMessage(content="what is the weather in adana now?")]},
+    )
+    for r in response["messages"]:
+        print(r.content)
